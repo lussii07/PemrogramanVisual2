e07c7e374d2b06bb3052dbc48ebc11cad5518475
lussii <122663468+lussii07@users.noreply.github.com>
2024-08-26 21:23:26 +0700

    Add files via upload
    Modul 4-7

---
+++ Coba2/src/main/java/com/mycompany/coba2/Coba2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coba2;
+
+/**
+ *
+ * @author luxxzy
+ */
+public class Coba2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Coba2/src/main/java/com/mycompany/coba2/Text_Hasil.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.coba2;
+
+/**
+ *
+ * @author luxxzy
+ */
+class Text_Hasil {
+
+    static void setText(String pilihan) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
